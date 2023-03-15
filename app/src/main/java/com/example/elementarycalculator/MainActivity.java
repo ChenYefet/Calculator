4a14b64886dbba61c97f2d89c9c8bd7497b2d2d1
Chen <chenyefet@hotmail.co.uk>
2023-03-15 03:33:01 +0000

    Create addition calculator
    * Used code from the following tutorial to create a calculator that adds two inputted numbers:
       - https://www.youtube.com/watch?v=dFlPARW5IX8
    * Edited code based on Android Studio suggestions

--- app/src/main/java/com/example/elementarycalculator/MainActivity.java
+++ app/src/main/java/com/example/elementarycalculator/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.elementarycalculator;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    @SuppressLint("DefaultLocale")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button add = findViewById(R.id.add_ButtonObject);
+        add.setOnClickListener(v ->
+        {
+            EditText firstNumber = findViewById(R.id.firstNumber_EditTextObject);
+            EditText secondNumber = findViewById(R.id.secondNumber_EditTextObject);
+            TextView result = findViewById(R.id.result_TextViewObject);
+
+            int firstNumberAsInteger = Integer.parseInt(firstNumber.getText().toString());
+            int secondNumberAsInteger = Integer.parseInt(secondNumber.getText().toString());
+            int resultAsInteger = firstNumberAsInteger + secondNumberAsInteger;
+            result.setText(String.format("%d", resultAsInteger));
+        });
     }
 }
